157fb5ebf9e7152c5c0a8db07defef2ad675e9c6
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-09-08 11:26:55 +0200

    Added ERole enum

---
+++ src/main/java/it/vscalcione/springboot/demoapplicationjwt/models/ERole.java
@@ -0,0 +1,24 @@
+package it.vscalcione.springboot.demoapplicationjwt;
+
+public enum ERole {
+
+    private final Integer id;
+    private final String description;
+
+    ROLE_USER(0, "USER ROLE"),
+    ROLE_MODERATOR(1, "MODERATOR ROLE"),
+    ROLE_ADMIN(2, "ADMIN ROLE");
+
+    ERole(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+};
